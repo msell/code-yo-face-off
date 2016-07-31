7a7ef3f80502f44740d71b2b8806254cdf8b9a5c
Matt Sell <mwsell@gmail.com>
2016-07-31 10:50:06 -0500

    first stab

--- src/focus.js
+++ src/focus.js
@@ -1,2 +1,12 @@
 #!/usr/bin/env node
 
+//import Player from 'player';
+
+// let player = new Player('../tracks/morning-murmur.mp3');
+
+// player.play((err, player) => {
+//   console.log('playend!');
+// })
+
+var player = require('play-sound')();
+player.play('./tracks/morning-murmur.mp3', function(err){});
