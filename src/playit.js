af1c05c9133b92d3f8271376265e8b7269fcc7e9
Matt Sell <mwsell@gmail.com>
2016-07-31 13:14:15 -0500

    enable looping

--- src/playit.js
+++ src/playit.js
@@ -1,7 +1,12 @@
 import player from 'play-sound';
 import path from 'path';
 
-export default (track) => {          
+const playit = (track, loop = false) => {          
   player().play(path.join(__dirname, track), (err) => {
+    if(loop){
+      playit(track, loop);
+    }
   });
 };
+
+export default playit;
